bdf3f85d8a8d715d3f52ac87dfb5dac7899c77e0
abdullahiabdullahi1002-cloud <abdullahi.abdullahi1002@gmail.com>
2025-10-19 21:32:29 +0100

    Adding S3 backend configuration for remote state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket       = "outline-terraform-state-abdullahi"
+    key          = "outline/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.16.0" # Maybe Add a version constraint to prevent breaking changes
+      version = "6.16.0"
     }
   }
 }
